5daffad1aa0dcd6383917f2756a0f5f9d3a62343
Seonwoo <wlstjsdn12@gmail.com>
2022-09-01 15:44:36 +0900

    Update axiosInstance.ts

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.PORT
-    ? "https://blueroom-info.herokuapp.com"
-    : "http://localhost:4000",
+  baseURL: "https://blueroom-info.herokuapp.com" || "http://localhost:4000",
 });
