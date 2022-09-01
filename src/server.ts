d9cde416fb6371461838021eb3c5a83cce948352
Seonwoo <wlstjsdn12@gmail.com>
2022-09-01 11:44:14 +0900

    2022-09-01

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
-import "./src/db";
+import "./db";
 import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
-import api from "./src/api";
 import cors from "@koa/cors";
 import serve from "koa-static";
 import send from "koa-send";
+import api from "./api";
 
 const app = new Koa();
 const router = new Router();
@@ -24,12 +24,12 @@ const corsOptions = {
 router.use("/api", api.routes());
 
 app.use(bodyParser());
-app.use(serve(__dirname + "/build"));
+app.use(serve(process.cwd() + "/build"));
 app.use(cors(corsOptions));
 app.use(router.routes()).use(router.allowedMethods());
 
 app.use(async (ctx) => {
-  await send(ctx, "index.html", { root: __dirname + "/build" });
+  await send(ctx, "index.html", { root: process.cwd() + "/build" });
 });
 
 app.listen(PORT, handleListening);
