465b4b916420540d9de34b2a2a2525dbaf0bdb99
Seonwoo <wlstjsdn12@gmail.com>
2022-09-06 15:32:41 +0900

    Update axiosInstance.ts

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -5,5 +5,5 @@ const development = "http://localhost:4000/";
 const url = process.env.NODE_ENV ? production : development;
 
 export const axiosInstance = axios.create({
-  baseURL: development,
+  baseURL: url,
 });
