e3bff855da183276c9a889dabd4ab900ab3368e0
Seonwoo <wlstjsdn12@gmail.com>
2022-09-14 17:07:38 +0900

    Update Admin.tsx

--- src/Routes/Admin.tsx
+++ src/Routes/Admin.tsx
@@ -688,9 +688,324 @@ function Admin() {
     }
   };
 
+  //   return (
+  //     <>
+  //       {isLoading ? null : data?.data ? (
+  //         <Form onSubmit={handleSubmit(onValid)}>
+  //           <Wrapper>
+  //             <Title>Admin</Title>
+  //             {/* <LabelWrapper>
+  //                   <Label htmlFor="title">Title</Label>
+  //                 </LabelWrapper>
+  //                 <Input
+  //                   {...register("title", { required: true })}
+  //                   placeholder="Title"
+  //                   id="title"
+  //                   value={title}
+  //                   onChange={(event) => {
+  //                     setTitle(event.currentTarget.value);
+  //                   }}
+  //                 ></Input> */}
+  //             {/* <LabelWrapper>
+  //               <Label>CreatedAt</Label>
+  //             </LabelWrapper>
+  //             <TextArea
+  //                   {...register("description", { required: true })}
+  //                   placeholder="Description"
+  //                   id="description"
+  //                   onChange={(event) => {
+  //                     setDescription(event.currentTarget.value);
+  //                   }}
+  //                 ></TextArea>
+  //             <TimeWrapper>
+  //                <Input
+  //                 {...register("createdAt", { required: true })}
+  //                 value={createdDate}
+  //                 type="date"
+  //                 style={{ marginTop: "10px" }}
+  //                 onChange={(event) => setCreatedDate(event.currentTarget.value)}
+  //               ></Input>
+  //              <Input
+  //                 {...register("createdTime", { required: true })}
+  //                 value={createdTime}
+  //                 type="time"
+  //                 style={{ marginTop: "10px" }}
+  //                 onChange={(event) => setCreatedTime(event.currentTarget.value)}
+  //               ></Input>
+  //             </TimeWrapper> */}
+  //           </Wrapper>
+  //           <div style={{ display: "flex" }}>
+  //             <NewsContainer>
+  //               <NewsTitleWrapper>
+  //                 <NewsTitle
+  //                   placeholder="Title"
+  //                   id="title"
+  //                   value={title}
+  //                   onChange={(event) => {
+  //                     setTitle(event.currentTarget.value);
+  //                   }}
+  //                 ></NewsTitle>
+  //                 <NewsTitleLogoWrapper>
+  //                   <NewsTitleLogo
+  //                     url={AllNfts[chain.toLowerCase()][project]?.logourl}
+  //                   ></NewsTitleLogo>
+  //                   <NewsTitleProjcetName>
+  //                     <Select
+  //                       {...register("chain", { required: true })}
+  //                       value={chain}
+  //                       id="chain"
+  //                       onChange={(event) => {
+  //                         setChain(event.currentTarget.value);
+  //                         setSelectedChain(event.currentTarget.value);
+  //                         if (event.currentTarget.value === "ETH") {
+  //                           setProject("cryptopunks");
+  //                         } else if (event.currentTarget.value === "SOL") {
+  //                           setProject("okaybears");
+  //                         } else if (event.currentTarget.value === "KLAY") {
+  //                           setProject("themetakongzklaytn");
+  //                         }
+  //                       }}
+  //                     >
+  //                       <option value="ETH">ETH</option>
+  //                       <option value="SOL">SOL</option>
+  //                       <option value="KLAY">KLAY</option>
+  //                     </Select>
+  //                     {"|"}
+  //                     <Select
+  //                       id="nft"
+  //                       value={project}
+  //                       {...register("nft", { required: true })}
+  //                       onChange={(event) => {
+  //                         setProject(event.currentTarget.value);
+  //                       }}
+  //                     >
+  //                       {selectedChain === "ETH" ? (
+  //                         <>
+  //                           <option value="cryptopunks">CryptoPunks</option>
+  //                           <option value="boredapeyachtclub">
+  //                             Bored-Ape-Yacht-Club
+  //                           </option>
+  //                           <option value="otherdeedforotherside">
+  //                             Otherdeed-for-Otherside
+  //                           </option>
+  //                           <option value="azuki">Azuki</option>
+  //                           <option value="clonex">CLONE-X</option>
+  //                         </>
+  //                       ) : null}
+  //                       {selectedChain === "SOL" ? (
+  //                         <>
+  //                           <option value="okaybears">Okay-Bears</option>
+  //                           <option value="degods">DeGods</option>
+  //                           <option value="trippinapetribe">
+  //                             Trippin-Ape-Tribe
+  //                           </option>
+  //                           <option value="cetsoncreck">Cets-on-Creck</option>
+  //                           <option value="primates">Primates</option>
+  //                         </>
+  //                       ) : null}
+  //                       {selectedChain === "KLAY" ? (
+  //                         <>
+  //                           <option value="themetakongzklaytn">
+  //                             THE-META-KONGZ-KLAYTN
+  //                           </option>
+  //                           <option value="metatoydragonz">
+  //                             Meta-Toy-DragonZ
+  //                           </option>
+  //                           <option value="sunmiyaclubofficial">
+  //                             Sunmmiya-Club-Official
+  //                           </option>
+  //                           <option value="sheepfarm">SheepFarm</option>
+  //                         </>
+  //                       ) : null}
+  //                     </Select>
+  //                   </NewsTitleProjcetName>
+  //                   <NewsTitleCreatedAt>
+  //                     <Input
+  //                       {...register("createdAt", { required: true })}
+  //                       value={createdDate}
+  //                       type="datetime-local"
+  //                       style={{
+  //                         marginTop: "10px",
+  //                         opacity: "0.6",
+  //                         border: "none",
+  //                         width: "auto",
+  //                       }}
+  //                       onChange={(event) => {
+  //                         setCreatedDate(
+  //                           event.currentTarget.value.slice(0, 10) +
+  //                             "T" +
+  //                             event.currentTarget.value.slice(11, 16)
+  //                         );
+  //                         setCreatedAt(
+  //                           event.currentTarget.value.slice(0, 10) +
+  //                             " " +
+  //                             event.currentTarget.value.slice(11, 16)
+  //                         );
+  //                       }}
+  //                     ></Input>
+  //                   </NewsTitleCreatedAt>
+  //                 </NewsTitleLogoWrapper>
+  //                 <NewSnsContainer>
+  //                   <Select
+  //                     {...register("SNS", { required: true })}
+  //                     value={sns}
+  //                     id="sns"
+  //                     onChange={(event) => {
+  //                       setSns(event.currentTarget.value);
+  //                     }}
+  //                   >
+  //                     <option value="Twitter">Twitter</option>
+  //                     <option value="Discord">Discord</option>
+  //                   </Select>
+  //                 </NewSnsContainer>
+  //               </NewsTitleWrapper>
+  //               <hr style={{ width: "100%" }}></hr>
+  //               <ReactQuill
+  //                 ref={(element) => {
+  //                   if (element !== null) {
+  //                     quillRef.current = element;
+  //                   }
+  //                 }}
+  //                 className="editor"
+  //                 theme="snow"
+  //                 value={descriptionValue}
+  //                 onChange={setDescriptionValue}
+  //                 formats={formats}
+  //                 modules={modules}
+  //               />
+  //             </NewsContainer>
+  //           </div>
+  //           <Wrapper>
+  //             <LabelWrapper>
+  //               <Label htmlFor="thumbnail">Thumbnail</Label>
+  //             </LabelWrapper>
+  //             <Input
+  //               {...register("thumbnail", { required: true })}
+  //               placeholder="Img URL"
+  //               id="thumbnail"
+  //               value={thumbnail}
+  //               onChange={(event) => {
+  //                 setThumbnail(event.currentTarget.value);
+  //               }}
+  //             ></Input>
+  //           </Wrapper>
+  //           <div
+  //             style={{
+  //               width: "100%",
+  //               display: "flex",
+  //               height: "auto",
+  //               justifyContent: "center",
+  //               alignItems: "center",
+  //             }}
+  //           >
+  //             <LeftHome>
+  //               <InfoContainer>
+  //                 <InfoWrapper>
+  //                   <Info>
+  //                     <InfoNonHover>
+  //                       <InfoImage url={thumbnail}></InfoImage>
+  //                       <InfoMain>
+  //                         <InfoMainLogo
+  //                           logourl={
+  //                             AllNfts[chain.toLowerCase()][project]?.logourl
+  //                           }
+  //                         ></InfoMainLogo>
+  //                         <InfoMainText>
+  //                           <InfoMainTitle>
+  //                             <div>{title}</div>
+  //                           </InfoMainTitle>
+  //                           <InfoMainSubText>
+  //                             <h1>
+  //                               {AllNfts[chain.toLowerCase()][project].title}
+  //                             </h1>
+  //                             <h1>{createdAt}</h1>
+  //                           </InfoMainSubText>
+  //                         </InfoMainText>
+  //                       </InfoMain>
+  //                     </InfoNonHover>
+  //                   </Info>
+  //                   <Info>
+  //                     <InfoNonHover>
+  //                       <InfoNonImage url={thumbnail}>
+  //                         <InfoImageContext>{title}</InfoImageContext>
+  //                         <InfoImageHashTag>#eth #event</InfoImageHashTag>
+  //                       </InfoNonImage>
+  //                       <InfoMain>
+  //                         <InfoMainLogo
+  //                           logourl={
+  //                             AllNfts[chain.toLowerCase()][project]?.logourl
+  //                           }
+  //                         ></InfoMainLogo>
+  //                         <InfoMainText>
+  //                           <InfoMainTitle>
+  //                             <div>{title}</div>
+  //                           </InfoMainTitle>
+  //                           <InfoMainSubText>
+  //                             <h1>
+  //                               {AllNfts[chain.toLowerCase()][project].title}
+  //                             </h1>
+  //                             <h1>{createdAt}</h1>
+  //                           </InfoMainSubText>
+  //                         </InfoMainText>
+  //                       </InfoMain>
+  //                     </InfoNonHover>
+  //                   </Info>
+  //                 </InfoWrapper>
+  //               </InfoContainer>
+  //             </LeftHome>
+  //             <InfoHover>
+  //               <InfoImage url={thumbnail}>
+  //                 <InfoImageContext>{title}</InfoImageContext>
+  //                 <InfoImageHashTag>#eth #event</InfoImageHashTag>
+  //               </InfoImage>
+  //               <InfoMain>
+  //                 <InfoMainLogo
+  //                   logourl={AllNfts[chain.toLowerCase()][project].logourl}
+  //                 ></InfoMainLogo>
+  //                 <InfoMainText>
+  //                   <InfoMainTitle>
+  //                     <div>{title}</div>
+  //                   </InfoMainTitle>
+  //                   <InfoMainSubText>
+  //                     <h1>{AllNfts[chain.toLowerCase()][project].title}</h1>
+  //                     <h1>{createdAt}</h1>
+  //                   </InfoMainSubText>
+  //                 </InfoMainText>
+  //               </InfoMain>
+  //             </InfoHover>
+  //           </div>
+  //           <div
+  //             style={{
+  //               width: "50%",
+  //               display: "flex",
+  //               flexDirection: "column",
+  //               justifyContent: "center",
+  //               alignItems: "center",
+  //             }}
+  //           >
+  //             <ErrorMessage>{errorMessage}</ErrorMessage>
+  //             <div
+  //               style={{
+  //                 width: "100%",
+  //                 display: "flex",
+  //                 justifyContent: "center",
+  //                 alignItems: "center",
+  //               }}
+  //             >
+  //               <Button>{isEdit ? "Edit" : "Upload"}</Button>
+  //             </div>
+  //           </div>
+  //         </Form>
+  //       ) : (
+  //         <PageNotFound />
+  //       )}
+  //     </>
+  //   );
+  // }
+
   return (
     <>
-      {isLoading ? null : data?.data ? (
+      {isLoading ? null : (
         <Form onSubmit={handleSubmit(onValid)}>
           <Wrapper>
             <Title>Admin</Title>
@@ -996,8 +1311,6 @@ function Admin() {
             </div>
           </div>
         </Form>
-      ) : (
-        <PageNotFound />
       )}
     </>
   );
