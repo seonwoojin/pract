5d9b76f70568f3df76963e1b007409c1f262868e
Seonwoo <wlstjsdn12@gmail.com>
2022-09-15 12:57:04 +0900

    Update axiosInstance.ts

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const production = "https://blueroom-info.herokuapp.com";
 const development = "http://localhost:4000/";
-const url = process.env.NODE_ENV ? production : development;
+const url = process.env.NODE_ENV !== "development" ? production : development;
+
+console.log(url);
 
 export const axiosInstance = axios.create({
-  baseURL: development,
+  baseURL: url,
 });
