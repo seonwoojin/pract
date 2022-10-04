a8a66c708bc1ba866426169b6c2bbb80ff45dcfc
Seonwoo <wlstjsdn12@gmail.com>
2022-10-04 10:37:30 +0900

    Update middlewares.ts

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -54,7 +54,6 @@ export const userChecker = async (
     ctx.user = user;
     await next(ctx);
   } catch (error) {
-    console.log(error);
     ctx.body = error;
     ctx.status = response.HTTP_BAD_REQUEST;
     return false;
